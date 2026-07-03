37eff476d5670d1538879b03751fe5f6e8ea8007
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-07-03 22:01:57 +0530

    Time: 0 ms (100%), Space: 43.6 MB (42.55%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -2,31 +2,56 @@ class Solution {
     public void sortColors(int[] nums) {
 
         int n = nums.length;
-        int countZero = 0, countOne = 0, countTwo = 0;
  // BRUTE FORCE : Count the no of elements and replace the existing array for the same time 
-        for(int i=0; i<n; i++){
-            if(nums[i]==0) countZero++;
-            if(nums[i]==1) countOne++;
-            if(nums[i]==2) countTwo++;
-        }
+    //     int countZero = 0, countOne = 0, countTwo = 0;
+    //     for(int i=0; i<n; i++){
+    //         if(nums[i]==0) countZero++;
+    //         if(nums[i]==1) countOne++;
+    //         if(nums[i]==2) countTwo++;
+    //     }
 
-        for(int i=0;i<n;i++){
-            while(countZero>0){
-                nums[i]=0;
-                i++;
-                countZero--;
-            }
-            while(countOne>0){
-                nums[i]=1;
-                i++;
-                countOne--;
-            }
-            while(countTwo>0){
-                nums[i]=2;
-                i++;
-                countTwo--;
-            }
-        }
+    //     for(int i=0;i<n;i++){
+    //         while(countZero>0){
+    //             nums[i]=0;
+    //             i++;
+    //             countZero--;
+    //         }
+    //         while(countOne>0){
+    //             nums[i]=1;
+    //             i++;
+    //             countOne--;
+    //         }
+    //         while(countTwo>0){
+    //             nums[i]=2;
+    //             i++;
+    //             countTwo--;
+    //         }
+    //     }
         
+    // }
+
+// OPTIMAL SOLN : DUTCH NATIONAL FLAG ALGORITHM
+    int low = 0, mid = 0, high = n-1;
+
+    while(mid <= high){
+        if(nums[mid] == 0){
+            int temp = nums[low];
+            nums[low] = nums[mid];
+            nums[mid] = temp;
+
+            low++;
+            mid++;
+        }
+        else if(nums[mid] == 1){
+            mid++;
+        }
+        else{
+            int temp = nums[high];
+            nums[high] = nums[mid];
+            nums[mid] = temp;
+
+            high--;
+        }
     }
+}
 }
