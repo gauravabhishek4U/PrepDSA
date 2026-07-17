8f33e52debd33438367061a671e71550388a515b
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-07-17 08:53:27 +0530

    Update 0904-fruit-into-baskets.java

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -4,9 +4,19 @@ class Solution {
         int ans = 0, n = fruits.length;
         int low=0, high =0;
 
+        // SLIDING WINDOW CREATION AND EXPANSION STEP
+        // store each element from high end in the map
+        // if already exist, get the frequency and increment by 1
+        // else initialise it with 0 and then increase the counter
+        
         for(high = 0; high<n; high++){
             map.put(fruits[high], map.getOrDefault(fruits[high], 0)+1);
 
+            
+            // SLIDING WINDOW SHRINKING PHASE
+            //if map size exceeds 2, then remove the element from the left end
+            // by getting the counter and decrementing it by 1
+            // finally move the left counter to next element
             while(map.size() > 2){
                 map.put(fruits[low], map.get(fruits[low]) -1);
                 if(map.get(fruits[low]) == 0){
@@ -14,6 +24,7 @@ class Solution {
                 }
                 low++;
             }
+            // calculating answer by maximising the length between two ends
             ans = Math.max(ans, high-low+1);
         }
         return ans;
