5280f78fc56b2da7ff7857a9d0aff831d8bcf09f
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-07-18 23:12:02 +0530

    Time: 4 ms (99.62%), Space: 47.7 MB (67.14%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,11 +1,22 @@
 class Solution {
     public boolean validPalindrome(String s) {
         int left = 0, right = s.length() - 1;
+
+
+        // Using two pointers method
+        // Approach : Whenever characters match, we move inward. At the first mismatch, we have the option to skip either the left or the right character to try forming a palindrome.
+        // TC :: O(N)
+        // SC :: O(1)
         while (left < right) {
             if (s.charAt(left) == s.charAt(right)) {
                 left++;
                 right--;
             } else {
+                // If s[left] != s[right], check if skipping left (s[left+1..right]) or right (s[left..right-1]) results in a palindrome using a helper function.
+
+                // If either skip works or no mismatches occur, return true; otherwise, return false.
+
+
                 return isPalindrome(s, left + 1, right) || isPalindrome(s, left, right - 1);
             }
         }
