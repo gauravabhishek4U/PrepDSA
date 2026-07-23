d1c784e3e24b9961ce1fb55b51fe382d5d72ac6e
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-07-24 02:59:10 +0530

    Time: 0 ms (100%), Space: 125.5 MB (27.45%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
@@ -1,3 +1,5 @@
+/*
+
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
         int n = nums.length;
@@ -11,5 +13,24 @@ class Solution {
             answer <<= 1;
 
         return answer;
+    }
+}
+
+*/
+
+// USING Bit Smearing
+
+class Solution {
+    public int uniqueXorTriplets(int[] nums) {
+        int n = nums.length;
+        int m = n;
+        
+        m |= m >> 1;
+        m |= m >> 2;
+        m |= m >> 4;
+        m |= m >> 8;
+        m |= m >> 16;
+        
+        return (m + 1) >> (3 / (n + 1));
     }
 }
