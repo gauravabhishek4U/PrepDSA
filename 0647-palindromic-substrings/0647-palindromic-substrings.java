5e4c424c2f4a0bf63c880aaf42005617dd5d137e
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-07-24 02:51:53 +0530

    Time: 5 ms (97.54%), Space: 42.9 MB (55.07%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,9 +1,12 @@
+/*
+
 class Solution {
     public int countSubstrings(String s) {
         int n = s.length();
         int count = 0;
 
 // BRUTE FORCE APPROACH :: Iterate through each substring and check if its palindrome
+// TC :: O(N^2) for iterating and N/2 for isPalindrome fnc..so O(N^3)/2 i.e., N cube divided by 2
         for(int i = 0; i<n; i++){
             for(int j = i; j<n; j++){
                 if(isPalindrome(s, i, j)){
@@ -24,4 +27,35 @@ class Solution {
         }
         return true;
     }
+}
+*/
+
+
+// OPTIMAL APPROACH :: TC = O(N) AND for expand fnc its N/2, 
+// so final TC :: O(N^2)
+
+class Solution {
+    public int countSubstrings(String s) {
+        int n = s.length();
+        int count = 0;
+
+        for(int i = 0; i<n; i++){
+            count += expand(s, i, i); // for odd length string
+            count += expand(s, i, i+1); // for even length string
+       
+        }
+        return count;
+    }
+
+    private int expand(String s, int left, int right){
+        int count = 0;
+
+        while(left >=0 && right < s.length() && s.charAt(left) == s.charAt(right)){
+            count++;
+            left--;
+            right++;
+        }
+        return count;
+    }
+
 }
