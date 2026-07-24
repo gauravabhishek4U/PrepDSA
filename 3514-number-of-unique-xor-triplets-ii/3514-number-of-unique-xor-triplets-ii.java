dc39112b35403d03be234b3e174ac3479b0c1eee
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-07-24 19:46:34 +0530

    Time: 207 ms (89.09%), Space: 47 MB (49.09%) - LeetHub

--- 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
+++ 3514-number-of-unique-xor-triplets-ii/3514-number-of-unique-xor-triplets-ii.java
@@ -1,31 +1,34 @@
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
-        final int MAX_XOR = 2048;
-
-        boolean[] pairXor = new boolean[MAX_XOR];
-        boolean[] tripletXor = new boolean[MAX_XOR];
-
         int n = nums.length;
 
+        int[] pairXor = new int[2048];
+        int[] tripleXor = new int[2048];
+
+        // Compute all possible pair XORs
         for (int i = 0; i < n; i++) {
             for (int j = i; j < n; j++) {
-                pairXor[nums[i] ^ nums[j]] = true;
+                pairXor[nums[i] ^ nums[j]] = 1;
             }
         }
 
-        for (int x = 0; x < MAX_XOR; x++) {
-            if (!pairXor[x]) continue;
-
-            for (int v : nums) {
-                tripletXor[x ^ v] = true;
+        // Generate triplet XORs
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < 2048; j++) {
+                if (pairXor[j] == 1) {
+                    tripleXor[j ^ nums[i]] = 1;
+                }
             }
         }
 
-        int count = 0;
-        for (boolean exists : tripletXor) {
-            if (exists) count++;
+        // Count distinct XOR values
+        int cnt = 0;
+        for (int value : tripleXor) {
+            if (value == 1) {
+                cnt++;
+            }
         }
 
-        return count;
+        return cnt;
     }
 }
