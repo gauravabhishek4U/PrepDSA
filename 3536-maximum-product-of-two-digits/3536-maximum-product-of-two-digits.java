1e2904b840d3bd5658e1e6c39202414dbb9de13f
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-07-25 23:10:56 +0530

    Time: 2 ms (29.84%), Space: 42.7 MB (38.93%) - LeetHub

--- 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
+++ 3536-maximum-product-of-two-digits/3536-maximum-product-of-two-digits.java
@@ -1,3 +1,4 @@
+/*
 class Solution {
     public int maxProduct(int n) {
 
@@ -16,6 +17,27 @@ class Solution {
             }
             n /= 10;
         }
-        return max1 * max2;
+        return max1 * max2;        
     }
 }
+*/
+
+// Brute Force Approach :: TC = O(n^2), SC = O(n)
+// Convert the integer into a string. Traverse every pair of digits using two nested loops.
+// Convert the characters into integers. Compute their product.
+// Store the maximum product.
+class Solution {
+    public int maxProduct(int n) {
+        int max = -1;
+        String str = n + "";
+        for(int i = 0 ; i < str.length() ; i++){
+            for(int j = i + 1 ; j < str.length() ; j++){
+                int d1 = str.charAt(i) - '0';
+                int d2 = str.charAt(j) - '0';
+                max = Math.max(max , d1 * d2);
+            }
+        }
+        return max;
+    }
+}
+
