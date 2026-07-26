007c01b733e7c4ace3e696e902296631be141916
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-07-26 23:12:27 +0530

    Time: 15 ms (80.39%), Space: 48.2 MB (25.98%) - LeetHub

--- 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.java
@@ -1,3 +1,8 @@
+
+/*
+// Optimal Approach : One Pass - Track 3 maximum and 2 minimum number
+// TC : O(N), SC : O(1)
+
 class Solution {
     public int maximumProduct(int[] nums) {
         int max1 = -1000, max2 = -1000, max3 = -1000;
@@ -31,5 +36,30 @@ class Solution {
             max1 * max2 * max3,
             min1 * min2 * max1
         );
+    }
+}
+
+*/
+
+// Approach : Sorting
+// Sorting the array in ascending order will give us 2 minimum elements at first two index
+// and 3 maximum element at the last three index of the array
+// Maximum product could only be either min1 * min2 * max1 or max1 * max2 * max3
+// TC = O(n log n), SC = O(log n)
+
+class Solution {
+    public int maximumProduct(int[] nums) {
+        int n = nums.length;
+
+        Arrays.sort(nums);
+
+        // product of two minimums and one greatest maximum
+        int product1 = nums[0] * nums[1] * nums[n-1];
+
+        // product of three maximums 
+        int product2 = nums[n-1] * nums[n-2] * nums[n-3];
+
+        return Math.max(product1 , product2);
+
     }
 }
