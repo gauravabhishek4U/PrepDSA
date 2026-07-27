6b48ebb63019724ffcf9711f67e5e3810945a229
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-07-27 15:01:11 +0530

    Time: 3 ms (48.8%), Space: 44.5 MB (60.68%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
@@ -19,6 +19,8 @@ class Solution {
 }
 */
 
+
+/*
 // Approach : Using Sorting
 // Sort the array in ascending order and calculate the product of last two elements 
 // TC = O(log n), SC = O(1)
@@ -28,5 +30,25 @@ class Solution{
         int n = nums.length;
         Arrays.sort(nums);
         return (nums[n-1]-1)*(nums[n-2]-1);
+    }
+}
+
+*/
+
+// Using Priority Queue. TC = O(n log 2) = O(n), SC = O(1)
+// Create a min-heap
+// Insert each element into the heap.
+// If the heap size becomes greater than 2, remove the smallest element.
+// Multiply the remaining two digits by subtracting 1 from them.
+class Solution {
+    public int maxProduct(int[] nums) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int num : nums){
+            pq.offer(num);
+            if(pq.size()>2){
+                pq.poll();
+            }
+        }
+        return (pq.poll()-1) * (pq.poll()-1);
     }
 }
