9677fc9b2662f9bb22142883ab4d8463b46eca70
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-07-31 02:41:03 +0530

    Time: 6 ms (1.45%), Space: 43.5 MB (16.67%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,3 +1,5 @@
+/*
+
 // Optimal Approach : Using hashmap
 // TC = O(n), SC = O(1)
 class Solution {
@@ -23,5 +25,40 @@ class Solution {
         }
         return result;
         
+    }
+}
+*/
+
+
+//Approach-2 (Simplifying Approach-1 above)
+//T.C : O(n) + O(26log26)
+//S.C : O(1)
+class Solution {
+    public int minimumPushes(String word) {
+
+        //edge case if word length is less than 8 (2,3,4,5,6,7,8,9)
+        int n = word.length();
+        if (n<=8){
+            return n;
+        }
+
+        int[] map = new int[26];
+        int ans = 0;
+
+        for(char ch : word.toCharArray()){
+            map[ch-'a']++;
+        }
+
+        // converting int array map to Integer Array
+        Integer[] mapInteger = Arrays.stream(map).boxed().toArray(Integer[] :: new);
+
+        // sorting in descending order based on the frequency of characters in the given string
+        Arrays.sort(mapInteger, Comparator.reverseOrder());
+
+        // calculating the total number of pushes required
+        for(int i=0; i<26; i++){
+            ans += mapInteger[i] * ((i/8)+1);
+        }
+        return ans;
     }
 }
