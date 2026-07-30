8c485c16b94e2db2c89d5ac258599ad3767c6cdb
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-07-31 02:07:32 +0530

    Time: 2 ms (21.26%), Space: 43.4 MB (16.67%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.java
@@ -1,37 +1,27 @@
-/*
-
-// word contains distinct lowercase english letters
-// each letter is mapped to only one key
-// so for first 8 letters : 1 push,
-// second set of 8 letters : 2 push
-// third set of 8 letters : 3 push
-// last 2 letters : 4 push
-// TC : O(1), SC = O(1)
+// Optimal Approach : Using hashmap
+// TC = O(n), SC = O(1)
 class Solution {
     public int minimumPushes(String word) {
-        int n = word.length();
-        int ans = 0;
-
-        if(n<=8)
-            ans = n;
-        else if(n<=16)
-            ans = 8 + (n-8)*2;
-        else if( n <= 24)
-            ans = 24 + (n-16)*3;
-        else 
-            ans = 48 + (n-24)*4;
         
-        return ans;
-    }
-}
-*/
-
-class Solution{
-    public int minimumPushes(String word){
-        int push = 0;
-        for(int i=0; i< word.length(); i++){
-            push += (i/8)+1;
+        if (word.length() <= 8) {
+            return word.length();
         }
-        return push;
+
+        int result = 0;
+        int assignKey = 2;
+        
+        Map <Integer, Integer> mpp = new HashMap<>();
+
+        for(char ch : word.toCharArray()){
+            if(assignKey > 9){
+                assignKey = 2;
+            }
+
+            mpp.put(assignKey, mpp.getOrDefault(assignKey,0)+1);
+            result += mpp.get(assignKey);
+            assignKey++;
+        }
+        return result;
+        
     }
 }
