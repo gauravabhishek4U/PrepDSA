425cfbb7720d9ee91104cb475abda3e508a799b6
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-08-01 09:21:13 +0530

    Time: 0 ms (100%), Space: 43.1 MB (10.58%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,16 +1,31 @@
 class Solution {
+    int n;
     public boolean predictTheWinner(int[] nums) {
-        return getNetScore(nums, 0, nums.length - 1) >= 0;
+        n = nums.length;
+        int[][] dp = new int[23][23];
+        for(int[] arr:dp){
+            Arrays.fill(arr, -1);
+        }
+        int totalSum = 0;
+        for(int num : nums){
+            totalSum+=num;
+        }
+        int first = solve(0, n-1, totalSum, nums, dp);
+        int second = totalSum-first;
+
+        if(first>=second) return true;
+        else{return false;}
+
     }
-    
-    private int getNetScore(int[] nums, int i, int j) {
-        if (i == j) 
-            return nums[i];
-    
-        
-        int pickLeft = nums[i] - getNetScore(nums, i + 1, j);
-        int pickRight = nums[j] - getNetScore(nums, i, j - 1);
-        
-        return Math.max(pickLeft, pickRight);
+    int solve(int i, int j, int total, int[] nums, int[][]dp){
+        if(i>j) return 0;
+        if(i==j) return nums[i];
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int take_i = nums[i]+ Math.min(solve(i+2, j, total, nums, dp) , solve(i+1, j-1, total, nums, dp));
+        int take_j = nums[j]+ Math.min(solve(i+1, j-1, total, nums, dp) , solve(i, j-2, total, nums, dp));
+
+        return dp[i][j]=  Math.max(take_i, take_j);
     }
 }
