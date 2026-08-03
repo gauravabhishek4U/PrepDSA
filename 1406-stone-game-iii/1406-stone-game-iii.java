62117f60c3707f12dbfbe85aef0114c89c92eef5
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-08-03 23:36:02 +0530

    Time: 5 ms (95.17%), Space: 85.4 MB (76.7%) - LeetHub

--- 1406-stone-game-iii/1406-stone-game-iii.java
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -1,43 +1,34 @@
 
-//Approach-1 (Using simple resurion + memoization)
+//Approach-2 (Using Bottom Up Approach)
 // TC = O(n)
 
 class Solution {
-    int n;
-    // initializing DP array
-    int[] t;
+   
 
     public String stoneGameIII(int[] stoneValue) {
-        n = stoneValue.length;
-        t = new int[n];
-        Arrays.fill(t, Integer.MIN_VALUE);
+        int n = stoneValue.length;
+        int[] t = new int[n + 1];
+        t[n] = 0;
 
-        int diff = solve(stoneValue, 0); // Alice - Bob
+        for(int i= n-1; i >= 0; i--){
+            t[i] = stoneValue[i] - t[i+1];
+
+            if(i+1 < n){
+                t[i] = Math.max(t[i], stoneValue[i] + stoneValue[i+1] - t[i+2]);
+            }
+
+            if(i+2 < n){
+                t[i] = Math.max(t[i], stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - t[i+3]);
+            }
+        }
+
+        int diff = t[0]; // Alice - Bob
 
         if(diff > 0) return "Alice";
         else if(diff < 0) return "Bob";
-        else return "Tie";
-        
-    }
 
-    private int solve(int[] stoneValue, int i){
-
-        if(i == n)
-            return 0;
+        return "Tie";
         
-        if(t[i] != Integer.MIN_VALUE)
-            {return t[i];}
-        
-        t[i] = stoneValue[i] - solve(stoneValue, i+1);
-        
-        if(i+1 < n)
-            t[i] = Math.max(t[i], stoneValue[i] + stoneValue[i+1] - solve(stoneValue, i+2));
-        
-        if(i+2 < n)
-            t[i] = Math.max(t[i], stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - solve(stoneValue, i+3));
-        
-        return t[i];
-
     }
 }
 
