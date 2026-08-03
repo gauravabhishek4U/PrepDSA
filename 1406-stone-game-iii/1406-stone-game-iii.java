3fa92c1c98d137a6e9337ef23724301c8d920436
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-08-03 22:09:35 +0530

    Time: 27 ms (56.25%), Space: 93.6 MB (28.41%) - LeetHub

---
+++ 1406-stone-game-iii/1406-stone-game-iii.java
@@ -0,0 +1,38 @@
+class Solution {
+    int n;
+    // initializing DP array
+    int[] t;
+
+    public String stoneGameIII(int[] stoneValue) {
+        n = stoneValue.length;
+        t = new int[n];
+        Arrays.fill(t, Integer.MIN_VALUE);
+
+        int diff = solve(stoneValue, 0);
+
+        if(diff > 0) return "Alice";
+        else if(diff < 0) return "Bob";
+        else return "Tie";
+        
+    }
+
+    private int solve(int[] stoneValue, int i){
+
+        if(i == n)
+            return 0;
+        
+        if(t[i] != Integer.MIN_VALUE)
+            {return t[i];}
+        
+        t[i] = stoneValue[i] - solve(stoneValue, i+1);
+        
+        if(i+1 < n)
+            t[i] = Math.max(t[i], stoneValue[i] + stoneValue[i+1] - solve(stoneValue, i+2));
+        
+        if(i+2 < n)
+            t[i] = Math.max(t[i], stoneValue[i] + stoneValue[i+1] + stoneValue[i+2] - solve(stoneValue, i+3));
+        
+        return t[i];
+
+    }
+}
