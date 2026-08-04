60ea90fb7e7e3a2805f195be16b572084404d751
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-08-04 14:02:59 +0530

    Time: 1 ms (100%), Space: 46.7 MB (67.71%) - LeetHub

--- 3731-find-missing-elements/3731-find-missing-elements.java
+++ 3731-find-missing-elements/3731-find-missing-elements.java
@@ -1,3 +1,4 @@
+/*
 // Approach 1 : Sorting
 // TC = O(n log n), SC = O(1)
 class Solution {
@@ -20,5 +21,37 @@ class Solution {
     }
 }
 
+*/
 
+// Optimal Approach : Binary Array
+// TC = O(n + R), where R = max-min+1
+// SC = O(1)
+
+// while traversing the array, we find minimum and maximum values
+// finally iterate from min to max and check if the number is marked or not
+// if not marked, then add it to the final answer list.
+
+class Solution{
+    public List<Integer> findMissingElements(int[] nums) {
+
+        List<Integer> ans = new ArrayList<>();
+        boolean[] contains = new boolean[101];
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for(int element : nums){
+            min = Math.min(min, element);
+            max = Math.max(max, element);
+            contains[element] = true;
+        }
+
+        for(int i=min; i<=max; i++){
+            if(!contains[i]){
+                ans.add(i);
+            }
+        }
+        return ans;
+    }
+}
 
