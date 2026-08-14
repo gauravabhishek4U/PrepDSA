c74aa0adb0a09d1cf02071f4be5d3be115177d01
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-08-14 16:50:47 +0530

    Time: 2 ms (63.69%), Space: 43.3 MB (63.47%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -0,0 +1,43 @@
+class Solution {
+    private List<String> result;
+    private Set<String> dict;
+
+    private void solve(int i, String currSentence, String s){
+
+        // case when i exceeds the length of s during incrementing
+        if(i >= s.length()){
+            result.add(currSentence.trim());
+            return;
+        }
+
+        for(int j = i; j < s.length(); j++){
+            String tempWord = s.substring(i, j+1);
+
+            // checking tempWord exists in the wordDict, it means it is a valid word
+            if(dict.contains(tempWord)){
+                String originalSentence = currSentence;
+
+                // if currSentence already has some word added to it
+                // if tempWord is not the first word, then we need to add space before it
+                if(!currSentence.isEmpty()){
+                    currSentence += " ";
+                }
+                currSentence += tempWord; // Do step of backtracking
+
+                solve(j+1, currSentence, s); // Explore step of backtracking
+                currSentence = originalSentence; // Undo step of backtracking
+            }
+        }
+    }
+
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        result = new ArrayList<>();
+        dict = new HashSet<>(wordDict);
+
+        String currSentence = "";
+
+        solve(0, currSentence, s);
+        return result;
+        
+    }
+}
