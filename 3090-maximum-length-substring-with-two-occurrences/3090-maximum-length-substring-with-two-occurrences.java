1433c0cea274d3122cc4496eac4f4c0eb2bbf8ef
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-08-14 13:07:21 +0530

    Time: 3 ms (43.02%), Space: 43.8 MB (47.03%) - LeetHub

--- 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
+++ 3090-maximum-length-substring-with-two-occurrences/3090-maximum-length-substring-with-two-occurrences.java
@@ -1,8 +1,11 @@
+// Approach : Sliding Window and HashMap
+// TC : O(n), SC = O(n)
 class Solution {
     public int maximumLengthSubstring(String s) {
         int n = s.length();
         int ans = Integer.MIN_VALUE;
 
+        // Base case when string s is just of size 2
         if(n == 2){
             ans=2;
             return ans;
@@ -11,20 +14,22 @@ class Solution {
         int left = 0;
         HashMap <Character, Integer> mpp = new HashMap<>();
 
+        // Expansion phase of sliding window and adding elements to map
         for(int right = 0; right < n; right++){
             char rightChar = s.charAt(right);
             mpp.put(rightChar, mpp.getOrDefault(rightChar, 0)+1);
             
+            // shrinking phase of sliding window
             while(mpp.get(rightChar) > 2){
                 char leftChar = s.charAt(left);
                 mpp.put(leftChar, mpp.get(leftChar)-1);
                 left++;
             }
 
+            // calculating the maximum possible answer
             ans = Math.max(ans, right-left+1);
 
         }
-        return ans;
-        
+        return ans;    
     }
 }
