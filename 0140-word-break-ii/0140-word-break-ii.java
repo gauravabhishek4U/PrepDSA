6fe4bb79822fcb6758525ddf08890a2a27ff38dc
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-08-14 23:55:25 +0530

    Time: 3 ms (56.69%), Space: 49 MB (12.89%) - LeetHub

--- 0140-word-break-ii/0140-word-break-ii.java
+++ 0140-word-break-ii/0140-word-break-ii.java
@@ -1,3 +1,4 @@
+/*
 //Approach-1 (Using simple backtracking)
 //T.C : O(2^n)
 //S.C : max depth of recursion is n (length of string s). You can say O(n) . You also have to store those sentences which is O(2^n)
@@ -43,5 +44,48 @@ class Solution {
         solve(0, currSentence, s);
         return result;
         
+    }
+}
+
+*/
+
+//Approach-2 (Using simple backtracking)
+//T.C : O(2^n)
+//S.C : max depth of recursion is n (length of string s). You can say O(n) . You also have to store those sentences which is O(2^n)
+public class Solution {
+    private Set<String> dict;
+    private Map<String, List<String>> memo;
+
+    private List<String> solve(String s) {
+        if (s.isEmpty()) {
+            return Arrays.asList("");
+        }
+        
+        if (memo.containsKey(s)) {
+            return memo.get(s);
+        }
+
+        List<String> result = new ArrayList<>();
+        for (int l = 1; l <= s.length(); l++) {
+            String currWord = s.substring(0, l);
+            if (dict.contains(currWord)) {
+                String remainWord = s.substring(l);
+                List<String> remainResult = solve(remainWord);
+                for (String w : remainResult) {
+                    String toAdd = currWord + (w.isEmpty() ? "" : " ") + w;
+                    result.add(toAdd);
+                }
+            }
+        }
+
+        memo.put(s, result);
+        return result;
+    }
+
+    public List<String> wordBreak(String s, List<String> wordDict) {
+        dict = new HashSet<>(wordDict);
+        memo = new HashMap<>();
+
+        return solve(s);
     }
 }
