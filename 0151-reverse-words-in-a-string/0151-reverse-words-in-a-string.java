04752b14b4bd789417a05789c998877a3a80875a
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-08-17 20:04:26 +0530

    Time: 6 ms (80.52%), Space: 44.9 MB (24.35%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -20,7 +20,11 @@ class Solution {
 
 */
 
+/*
 // Approach 2 : Using iteration and in-Built Methods
+// TC = O(N)
+// SC = O(N)
+
 class Solution {
     public String reverseWords(String s) {
 
@@ -42,5 +46,41 @@ class Solution {
 
         // returning the final ans string by trimming any whitespaces
         return ans.toString().trim();
+    }
+}
+
+*/
+
+
+// Approach 3 : two pointer and in built methods
+// TC = O(n)
+// SC = O(n)
+
+class Solution {
+    public String reverseWords(String s) {
+        String[] words = s.split(" ");
+        int left = 0;
+        int right = words.length - 1;
+
+        // reversing the string using two pointer
+        while (left < right) {
+            String temp = words[left];
+            words[left] = words[right];
+            words[right] = temp;
+            left++;
+            right--;
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (String word : words) {
+            if (!word.isEmpty()) {
+                if (result.length() > 0) {
+                    result.append(" ");
+                }
+                result.append(word);
+            }
+        }
+
+        return result.toString();
     }
 }
