8afaff96a02ab98b0af7d88f8bb86da836debff4
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-08-17 16:18:10 +0530

    Time: 361 ms (13.7%), Space: 47.5 MB (87.74%) - LeetHub

--- 1563-stone-game-v/1563-stone-game-v.java
+++ 1563-stone-game-v/1563-stone-game-v.java
@@ -1,58 +1,33 @@
-// Approach : Recursion + Memoization
-// TC = O(N^3)
-// SC = O(N^2)
-
+//Approach-2 - Bottom Up (Tabulation)
+//T.C : O(n^3)
+//S.C : O(n^2)
 class Solution {
-
-    int[][] t = new int[501][501];
-
-    public int solve(int l, int r, int[] cumSum){
-        if(l >= r){
-            return 0; // score is zero, furthur division is not posible
-        }
-
-        if(t[l][r] != -1){
-            return t[l][r];
-        }
-
-        int score = 0;
-
-        for(int mid = l; mid <= r-1; mid++){
-
-            // calculating sum from l to mid [l...mid]
-            int leftSum = cumSum[mid] - (l-1 >=0 ? cumSum[l-1] : 0); 
-
-            // calculating sum from mid+1 to r [mid+1...r]
-            int rightSum = cumSum[r] - cumSum[mid];
-
-            if(leftSum < rightSum){
-                score = Math.max(score, leftSum + solve(l, mid, cumSum));
-            }
-            else if(leftSum > rightSum){
-                score = Math.max(score, rightSum + solve(mid+1, r, cumSum));
-            }
-            else{
-                score = Math.max(score, Math.max(leftSum + solve(l, mid, cumSum), rightSum + solve(mid+1, r, cumSum)));
-            }
-        }
-        return t[l][r] = score;
-    }
-
-
     public int stoneGameV(int[] stoneValue) {
         int n = stoneValue.length;
         int[] cumSum = new int[n];
-
         cumSum[0] = stoneValue[0];
-
-        // calculating the cumulative sum
-        for(int i=1; i<n; i++){
-            cumSum[i] = cumSum[i-1] + stoneValue[i];
+        for (int i = 1; i < n; i++) {
+            cumSum[i] = cumSum[i - 1] + stoneValue[i];
         }
-
-        for(int[] row : t){
-            Arrays.fill(row, -1);
+        int[][] t = new int[n+1][n+1];
+        for(int l = n-1; l >= 0; l--) {
+            for(int r = l+1; r < n; r++) {
+                //l...r
+                int score = 0;
+                for(int mid = l; mid <= r-1; mid++) {
+                    int leftSum  = cumSum[mid] - (l-1 >= 0 ? cumSum[l-1] : 0); //[l..mid]
+                    int rightSum = cumSum[r] - cumSum[mid]; //mid+1, r
+                    if(leftSum < rightSum) {
+                        score = Math.max(score, leftSum + t[l][mid]);
+                    } else if(leftSum > rightSum) {
+                        score = Math.max(score, rightSum + t[mid+1][r]);
+                    } else {
+                        score = Math.max(score, Math.max(leftSum + t[l][mid], rightSum + t[mid+1][r]));
+                    }
+                }
+                t[l][r] = score;
+            }
         }
-        return solve(0, n-1, cumSum);
+        return t[0][n-1];
     }
 }
