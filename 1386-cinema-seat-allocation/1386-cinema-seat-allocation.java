c16e3914ed77dc62c2132c095931feeb832feb13
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-08-19 17:02:03 +0530

    Time: 25 ms (47.11%), Space: 57.2 MB (12.89%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -1,33 +1,38 @@
-//Approach-2 (Greedy Pick using bit mask)
+//Approach-3 (Greedy Pick using map and arraylist)
 //T.C : O(N), N = reservedSeats.length
-//S.C : O(N), for storing reserved seats in the form of mask
+//S.C : O(N), for storing reserved seats
 class Solution {
     public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
-        Map<Integer, Integer> mp = new HashMap<>(); //row -> bitmask of booked seats
+        Map<Integer, ArrayList<Integer>> mp = new HashMap<>(); //row -> bitmask of booked seats
 
-        for(int[] reservedSeat : reservedSeats) { //O(10^4)
-            int row  = reservedSeat[0];
-            int seat = reservedSeat[1];
-            mp.merge(row, (1 << seat), (a, b) -> a | b); //set bits are the booked seats
+        // store row wise reservedSeats
+        for(int[] seat : reservedSeats) { //O(10^4)
+            mp.putIfAbsent(seat[0], new ArrayList<>());
+            mp.get(seat[0]).add(seat[1]);
         }
 
+        // Row without any reservation can have 2 families i.e., group A and group C
         int result = (n - mp.size()) * 2;
 
-        int maskA = (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5); //set bits are the ones I need empty for Group A
-        int maskB = (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7); //set bits are the ones I need empty for Group B
-        int maskC = (1 << 6) | (1 << 7) | (1 << 8) | (1 << 9); //set bits are the ones I need empty for Group C
+        // check only rows having reservations
+        for(int row : mp.keySet()){
+            boolean A = true;
+            boolean B = true;
+            boolean C = true;
+            
+            for(int seat : mp.get(row)){
 
-        for(Map.Entry<Integer, Integer> entry : mp.entrySet()) { //min(10*n, 10^4)
-            int bookedSeatsMask = entry.getValue();
+                if(seat >= 2 && seat <= 5)
+                    A = false;
+                if(seat >= 4 && seat <= 7)
+                    B = false;
+                if(seat >= 6 && seat <= 9)
+                    C = false;
+            }
 
-            boolean groupA = (bookedSeatsMask & maskA) == 0;
-            boolean groupB = (bookedSeatsMask & maskB) == 0;
-            boolean groupC = (bookedSeatsMask & maskC) == 0;
+            if(A && C) result += 2;
 
-            if(groupA && groupC)
-                result += 2;
-            else if(groupA || groupB || groupC)
-                result += 1;
+            else if( A || B || C) result += 1; 
         }
 
         return result;
