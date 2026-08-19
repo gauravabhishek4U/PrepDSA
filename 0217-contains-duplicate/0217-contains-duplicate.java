a81148352b58a5adeada0d77d2f6dc361fc2bf1d
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-08-19 19:00:32 +0530

    Time: 39 ms (5.1%), Space: 110.5 MB (8.57%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,3 +1,4 @@
+/*
 // Approach : Using HashSet
 // TC = O(N)
 // SC = O(N)
@@ -9,6 +10,30 @@ class Solution {
         }
 
         return (nums.length == stt.size() ? false : true);
+        
+    }
+}
+*/
+
+// Approach : Using HashMap
+// TC = O(N)
+// SC = O(N)
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0)+1);
+        }
+
+        // iterating the map using only values
+        for(Integer value : map.values()){
+            if (value > 1){
+                return true;
+            }
+        }
+
+        return false;
         
     }
 }
