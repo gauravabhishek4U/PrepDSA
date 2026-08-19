0e1a8f7149cc1b60da6a7101d1acb10bb5606785
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-08-19 16:22:48 +0530

    Time: 17 ms (91.32%), Space: 53.6 MB (29.74%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -1,34 +1,35 @@
-// Approach : Greedy pick using Map
-// TC = O(N), N = reservedSeats.length
-// SC = O(N), for storing reservedSeats in HashMap
-
+//Approach-2 (Greedy Pick using bit mask)
+//T.C : O(N), N = reservedSeats.length
+//S.C : O(N), for storing reserved seats in the form of mask
 class Solution {
     public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
-        Map<Integer, Set<Integer>> map = new HashMap<>();
+        Map<Integer, Integer> mp = new HashMap<>(); //row -> bitmask of booked seats
 
-        for(int[] reservedSeat : reservedSeats){
-            int row = reservedSeat[0];
+        for(int[] reservedSeat : reservedSeats) { //O(10^4)
+            int row  = reservedSeat[0];
             int seat = reservedSeat[1];
-
-            map.computeIfAbsent(row, value -> new HashSet<>()).add(seat);
+            mp.merge(row, (1 << seat), (a, b) -> a | b); //set bits are the booked seats
         }
 
-        int result = (n - map.size()) * 2;
+        int result = (n - mp.size()) * 2;
 
-        for(Map.Entry<Integer, Set<Integer>> entry : map.entrySet()){
-            Set<Integer> bookedSeats = entry.getValue();
+        int maskA = (1 << 2) | (1 << 3) | (1 << 4) | (1 << 5); //set bits are the ones I need empty for Group A
+        int maskB = (1 << 4) | (1 << 5) | (1 << 6) | (1 << 7); //set bits are the ones I need empty for Group B
+        int maskC = (1 << 6) | (1 << 7) | (1 << 8) | (1 << 9); //set bits are the ones I need empty for Group C
 
-            boolean grpA = !bookedSeats.contains(2) && !bookedSeats.contains(3) && !bookedSeats.contains(4) && !bookedSeats.contains(5);
+        for(Map.Entry<Integer, Integer> entry : mp.entrySet()) { //min(10*n, 10^4)
+            int bookedSeatsMask = entry.getValue();
 
-            boolean grpB = !bookedSeats.contains(4) && !bookedSeats.contains(5) && !bookedSeats.contains(6) && !bookedSeats.contains(7);
+            boolean groupA = (bookedSeatsMask & maskA) == 0;
+            boolean groupB = (bookedSeatsMask & maskB) == 0;
+            boolean groupC = (bookedSeatsMask & maskC) == 0;
 
-            boolean grpC = !bookedSeats.contains(6) && !bookedSeats.contains(7) && !bookedSeats.contains(8) && !bookedSeats.contains(9);
-
-            if(grpA && grpC)
+            if(groupA && groupC)
                 result += 2;
-            else if(grpA || grpB || grpC)
+            else if(groupA || groupB || groupC)
                 result += 1;
         }
+
         return result;
     }
 }
