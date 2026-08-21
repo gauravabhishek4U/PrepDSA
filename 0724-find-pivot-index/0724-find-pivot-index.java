399746051ac964a25a79ad9409f796323d652086
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-08-21 18:17:08 +0530

    Time: 1 ms (97.52%), Space: 47.3 MB (60.54%) - LeetHub

---
+++ 0724-find-pivot-index/0724-find-pivot-index.java
@@ -0,0 +1,31 @@
+/*
+Approach : Prefix and Suffix Sum
+TC = O(n), n = nums.length
+SC = O(1)
+*/
+
+class Solution {
+    public int pivotIndex(int[] nums) {
+        
+        int n = nums.length;
+        int totalSum = 0;
+        int leftSum = 0;
+
+        // calculate the total sum of all elements in nums
+        for(int num : nums){
+            totalSum += num;
+        }
+
+        // calculate prefix and suffix sum for every index and check if they are equal
+        for(int i = 0; i < n; i++){
+
+            int rightSum = totalSum - leftSum - nums[i];
+
+            if(leftSum == rightSum){
+                return i;
+            }
+            leftSum += nums[i];
+        }
+        return -1;
+    }
+}
