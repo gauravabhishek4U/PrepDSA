2836fe4110aac962f13b2e62258a519183b2bcf1
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-08-25 19:40:40 +0530

    Time: 0 ms (100%), Space: 44.9 MB (76.35%) - LeetHub

--- 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
+++ 3718-smallest-missing-multiple-of-k/3718-smallest-missing-multiple-of-k.java
@@ -2,7 +2,7 @@
 Approach: HashSet
 TC = O(N)
 SC = O(N)
-*/
+
 class Solution {
     public int missingMultiple(int[] nums, int k) {
         Set<Integer> seen = new HashSet<>();
@@ -16,5 +16,35 @@ class Solution {
         }
 
         return cur;
+    }
+}
+
+*/
+
+/*
+Approach : Array Search
+TC = O(N^2)
+SC = O(1)
+*/
+
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+
+        // checking for multiples of k
+        for (int value = k; ; value += k) {
+            boolean found = false;
+
+            // checking for each element present in nums if it equals the multiple of k
+            for (int num : nums) {
+                if (num == value) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found) {
+                return value;
+            }
+        }
     }
 }
