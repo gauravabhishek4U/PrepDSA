d887f7d5bbd18699f2591c2b4dd850add3454054
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-08-27 01:38:18 +0530

    Time: 2 ms (99.92%), Space: 52.4 MB (17.73%) - LeetHub

--- 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
+++ 1004-max-consecutive-ones-iii/1004-max-consecutive-ones-iii.java
@@ -2,23 +2,25 @@ class Solution {
     public int longestOnes(int[] nums, int k) {
         int left=0 , right=0, zeros=0;
         int maxCount = 0, n = nums.length;
-
+/*
 // Approach 1 (Better Soln): counting no of zeros by managing a zero counter
 // TC : more than O(N) but lesser than O(N^2) : it is O(2N)
 // here right pointer waits until left pointer is moving 
-        // for(right = 0; right < n; right++){
-        //     if(nums[right] == 0){
-        //         zeros++;
-        //     }
-        //     while(zeros>k){
-        //         if(nums[left] == 0){
-        //             zeros--;
-        //         }
-        //         left++;
-        //     }
-        //     maxCount = Math.max(maxCount, right-left+1);
-        // }
-        // return maxCount;
+
+        for(right = 0; right < n; right++){
+            if(nums[right] == 0){
+                zeros++;
+            }
+            while(zeros>k){
+                if(nums[left] == 0){
+                    zeros--;
+                }
+                left++;
+            }
+            maxCount = Math.max(maxCount, right-left+1);
+        }
+        return maxCount;
+*/
 
 // Approach 2nd : Optimal Soln : left and right pointer moves together
 // TC : O(N)
@@ -35,7 +37,7 @@ class Solution {
                 left++;
             }
         }
-        return n -left;
+        return n - left;
 
     }
 }
