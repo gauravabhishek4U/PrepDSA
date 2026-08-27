61e94ad611aa8be7cc72c64e3d9dee49337d2809
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-08-28 03:19:23 +0530

    Time: 2 ms (70.91%), Space: 55.7 MB (46.57%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,3 +1,11 @@
+/*
+Approach 1 : using hashmap
+TC = O(N)
+SC = O(N), for storing entire N elements of nums in hashMap
+Solved : 1
+*/
+
+/*
 class Solution {
     public int majorityElement(int[] nums) {
 
@@ -22,5 +30,32 @@ class Solution {
             }
         }
          return ans;
+    }
+}
+*/
+
+/*
+Approach 2 : Moore Voting Algorithm, using two variables
+TC: O(N)
+SC = O(1)
+*/
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int count = 0, result = 0;
+
+        for(int num : nums){
+            if(count == 0){
+                result = num;
+            }
+
+            if(num != result){
+                count--;
+            }
+            else{
+                count++;
+            }
+        }
+        return result;
     }
 }
