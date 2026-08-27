32903910edee21ffdcdbcea9a20359220dfcefeb
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-08-27 19:38:05 +0530

    Time: 56 ms (29.66%), Space: 70.6 MB (28.86%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.java
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.java
@@ -1,30 +1,31 @@
+/*
+Approach : Sliding Window
+TC = O(N)
+SC = O(N)
+Solved : 3
+*/
+
 class Solution {
     public int totalFruit(int[] fruits) {
         Map<Integer, Integer> map = new HashMap<>();
         int ans = 0, n = fruits.length;
         int low=0, high =0;
 
-        // SLIDING WINDOW CREATION AND EXPANSION STEP
-        // store each element from high end in the map
-        // if already exist, get the frequency and increment by 1
-        // else initialise it with 0 and then increase the counter
-        
         for(high = 0; high<n; high++){
             map.put(fruits[high], map.getOrDefault(fruits[high], 0)+1);
 
-            
-            // SLIDING WINDOW SHRINKING PHASE
-            //if map size exceeds 2, then remove the element from the left end
-            // by getting the counter and decrementing it by 1
-            // finally move the left counter to next element
+            // if basket size is greater than 2, remove the fruit from left end
             while(map.size() > 2){
+
                 map.put(fruits[low], map.get(fruits[low]) -1);
+
+                // if freq of that fruit is 0, then remove it from the map (basket)
                 if(map.get(fruits[low]) == 0){
                     map.remove(fruits[low]);
                 }
+                
                 low++;
             }
-            // calculating answer by maximising the length between two ends
             ans = Math.max(ans, high-low+1);
         }
         return ans;
