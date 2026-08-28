dc35f0158d97e924ecfda026f2e3a4677cb04e14
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-08-29 01:55:12 +0530

    Time: 0 ms (100%), Space: 42.9 MB (58.39%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.java
@@ -1,37 +1,42 @@
 /*
-Approach-2 (using counting only. No map required)
+Approach-3 (using frequency array and counting)
 T.C : O(n)
-S.C : O(1)
-Solved : 2
+S.C : O(26) ~= O(1)
+Solved : 3
 */
 class Solution {
     public boolean areAlmostEqual(String s1, String s2) {
         int n = s1.length();
 
+        // Base case : if both strings are equal., no swapping is needed
         if (s1.equals(s2)) {
             return true;
         }
 
         int diffs = 0;
 
-        int firstIdx = 0;
-        int secondIdx = 0;
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
 
         for (int i = 0; i < n; i++) {
             if (s1.charAt(i) != s2.charAt(i)) {
                 diffs++;
+            }
 
-                if (diffs > 2) {
-                    return false;
-                } else if (diffs == 1) {
-                    firstIdx = i;
-                } else {
-                    secondIdx = i;
-                }
+            if (diffs > 2)
+                return false;
+
+            freq1[s1.charAt(i) - 'a']++;
+            freq2[s2.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < 26; i++) {
+            if (freq1[i] != freq2[i]) {
+                return false;
             }
         }
 
-        // checking if characters at two different indexes are same in s1 and s2
-        return s1.charAt(firstIdx) == s2.charAt(secondIdx) && s1.charAt(secondIdx) == s2.charAt(firstIdx);
+        return true;
     }
 }
+
