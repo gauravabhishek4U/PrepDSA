caf9e9b30b9ac50b5e93a8477eadbb5ef1cb9c51
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-08-29 01:11:09 +0530

    Time: 3 ms (21.22%), Space: 47.4 MB (34.2%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,3 +1,39 @@
+/*
+Approach : Kadane's Algorithm
+TC = O(N)
+SC = O(1)
+Solve: 2
+*/
+
+class Solution{
+    public int maxProduct(int[] nums) {
+        int n = nums.length;
+        int bestMax = nums[0];
+        int bestMin = nums[0];
+        int ans = nums[0];
+
+        for(int i = 1; i< n; i++){
+
+            // alone as prod
+            int prod1 = nums[i];
+
+            // pair up with existing best
+            int prod2 = nums[i] * bestMax;
+            int prod3 = nums[i] * bestMin;
+
+            // we have two best endings as bestMax and bestMin, so update both
+            bestMax = Math.max(prod1, Math.max(prod2, prod3));
+            bestMin = Math.min(prod1, Math.min(prod2, prod3));
+
+            // calculate the ans as the maximum of all three choices
+            ans = Math.max(ans, Math.max(bestMax, bestMin));
+        }
+
+        return ans;
+    }
+}
+
+/*
 // Approach :: using Prefix and Suffix Product
 // TC = O(N), SC = O(1)
 
@@ -23,7 +59,7 @@ class Solution {
         return maxProduct;
     }
 }    
-
+*/
 
 /*
 // Approach :: Using Kadane's Algo
