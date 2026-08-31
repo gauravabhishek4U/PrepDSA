ba0ab5db78366c33ac92d7150c3bd79146948502
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-08-31 21:02:54 +0530

    Time: 93 ms (33.66%), Space: 246.4 MB (5.03%) - LeetHub

--- 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
+++ 2948-make-lexicographically-smallest-array-by-swapping-elements/2948-make-lexicographically-smallest-array-by-swapping-elements.java
@@ -1,3 +1,44 @@
+//Approach-2 (using sorting and grouping using unordered_map)
+//T.C : ~O(n*logn)
+//S.C : ~O(n)
+
+class Solution {
+    public int[] lexicographicallySmallestArray(int[] nums, int limit) {
+        
+        int n = nums.length;
+        int[] vec = nums.clone();
+        Arrays.sort(vec);
+
+        int groupNum = 0;
+        Map<Integer, Integer> numToGroup = new HashMap<>();
+        Map<Integer, LinkedList<Integer>> groupToList = new HashMap<>();
+
+        numToGroup.put(vec[0], groupNum);
+        groupToList.putIfAbsent(groupNum, new LinkedList<>());
+        groupToList.get(groupNum).add(vec[0]);
+
+        for (int i = 1; i < n; i++) {
+            if (Math.abs(vec[i] - vec[i - 1]) > limit) {
+                groupNum++;
+            }
+            numToGroup.put(vec[i], groupNum);
+            groupToList.putIfAbsent(groupNum, new LinkedList<>());
+            groupToList.get(groupNum).add(vec[i]);
+        }
+
+        int[] result = new int[n];
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            int group = numToGroup.get(num);
+            result[i] = groupToList.get(group).pollFirst(); // Use and remove the smallest element
+        }
+
+        return result;
+    }
+}
+
+
+
 /*
 Approach-1 (Brute Force) gives TLE
 T.C : O(n^3)
@@ -37,40 +78,3 @@ class Solution {
     }
 }
 */
-
-//Approach-2 (using sorting and grouping using unordered_map)
-//T.C : ~O(n*logn)
-//S.C : ~O(n)
-class Solution {
-    public int[] lexicographicallySmallestArray(int[] nums, int limit) {
-        int n = nums.length;
-        int[] vec = nums.clone();
-        Arrays.sort(vec);
-
-        int groupNum = 0;
-        Map<Integer, Integer> numToGroup = new HashMap<>();
-        Map<Integer, LinkedList<Integer>> groupToList = new HashMap<>();
-
-        numToGroup.put(vec[0], groupNum);
-        groupToList.putIfAbsent(groupNum, new LinkedList<>());
-        groupToList.get(groupNum).add(vec[0]);
-
-        for (int i = 1; i < n; i++) {
-            if (Math.abs(vec[i] - vec[i - 1]) > limit) {
-                groupNum++;
-            }
-            numToGroup.put(vec[i], groupNum);
-            groupToList.putIfAbsent(groupNum, new LinkedList<>());
-            groupToList.get(groupNum).add(vec[i]);
-        }
-
-        int[] result = new int[n];
-        for (int i = 0; i < n; i++) {
-            int num = nums[i];
-            int group = numToGroup.get(num);
-            result[i] = groupToList.get(group).pollFirst(); // Use and remove the smallest element
-        }
-
-        return result;
-    }
-}
