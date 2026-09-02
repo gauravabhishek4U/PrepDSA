5964a29f98cc4dbcdccf326cf3ac90fe2bc32a0e
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-09-02 15:53:51 +0530

    Time: 0 ms (100%), Space: 45.3 MB (35.83%) - LeetHub

--- 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
+++ 3875-construct-uniform-parity-array-i/3875-construct-uniform-parity-array-i.java
@@ -2,7 +2,15 @@
 Approach : Odd-Even Parity
 TC : O(1)
 SC : O(1)
+
+If all numbers are even → already valid.
+If all numbers are odd → already valid.
+If both odd and even exist → subtract an opposite-parity number to change the parity.
+So it is always possible.
+The answer is Always true.
+
 */
+
 class Solution {
     public boolean uniformArray(int[] nums1) {
         return true;
