4ab475c12cc9060ee15ec9df8fe5cea2f4227081
Abhishek Gaurav <gauravabhishek400@gmail.com>
2026-09-05 07:40:25 +0530

    Time: 3 ms (100%), Space: 133.3 MB (20.99%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -2,7 +2,7 @@
 Approach : Suffix minimum and prefix max
 TC : O(N)
 SC = O(1)
-Solved = 1
+Solved = 2
 */
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
@@ -11,20 +11,22 @@ class Solution {
         int[] suffixMin = new int[n];
         
         int min = Integer.MAX_VALUE;
+        int prefixMax = Integer.MIN_VALUE;
 
+        // create suffix min for each index
         for(int i = n-1; i >= 0; i--){
             min = Math.min(min, nums[i]);
             suffixMin[i] = min;
         }
 
-        int prefixMax = Integer.MIN_VALUE;
-
+        // create prefix max and stable index for each index
         for(int i = 0; i<n; i++){
             prefixMax = Math.max(prefixMax, nums[i]);
             int index = prefixMax - suffixMin[i];
             if(index <= k)
             return i;
         }
+
         return -1;
     }
 }
